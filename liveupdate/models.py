3bbb6f8d2fb140cc08f833616221f3d24908c9a7
zhongwuzw <zhongwuzw@qq.com>
2014-04-29 17:25:27 +0800

    测试基于User的用户类别

--- liveupdate/models.py
+++ liveupdate/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django import forms
 # Create your models here.
 class Update(models.Model):
     timestamp = models.DateTimeField(auto_now_add = True)
@@ -13,3 +15,34 @@ class Update(models.Model):
                          self.text)
         
 admin.site.register(Update)
+
+class Contact(models.Model):
+    user = models.OneToOneField(User)
+    address = models.CharField(max_length = 255)
+    
+    def __unicode__(self):
+        return self.user.get_full_name()
+    
+    @property
+    def first_name(self):
+        return self.user.first_name
+    
+    @property
+    def last_name(self):
+        return self.user.last_name
+    
+    @property
+    def get_full_name(self):
+        return self.user.get_full_name()
+    
+admin.site.register(Contact)
+
+class UserEditorForm(forms.ModelForm):
+    class Meta:
+        model = User
+        field = ('username','first_name','last_name','email')
+        
+class ContactEditorForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        exclude = ('user',)
--- liveupdate/views.py
+++ liveupdate/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from liveupdate.models import Update
+from liveupdate.models import Update,Contact,ContactEditorForm,UserEditorForm
 from django.views.generic.base import TemplateView
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.core import serializers
-
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 class HomeListView(ListView):
     template_name = 'update_list.html'
@@ -18,5 +20,42 @@ class UpdateAfterView(TemplateView):
             response['Content-Type'] = 'text/javascript'
             response.write(serializers.serialize("json", Update.objects.filter(pk__gt = kwargs['id'])))
             return response
+      
+class EditContact(TemplateView):
+    def get_objects(self,username):
+        if username:
+            user = get_object_or_404(User,username = username)
+            try:
+                contact = user.contact
+            except Contact.DoesNotExist:   
+                contact = Contact(user = user)
+        else:
+            user = User()
+            contact = Contact(user = user)
             
+        return user,contact
     
+    def get(self,request,username = None):
+        user,contact = self.get_objects(username)
+        
+        return self.render_to_response({'username':username,
+                                        'user_form':UserEditorForm(instance = user),
+                                        'contact_form':ContactEditorForm(instance = contact),
+                                        })
+    
+    def post(self,request,username = None):
+        user,contact = self.get_objects(username)
+        
+        user_form = UserEditorForm(request.POST,instance = user)
+        contact_form = ContactEditorForm(request.POST,instance = contact)
+        
+        if user_form.is_valid() and contact_form.is_valid():
+            user = user_form.save()
+            contact = contact_form.save(commit = False)
+            contact.user = user
+            contact.save()
+            return HttpResponseRedirect(reverse('contact_detail',kwargs = {'slug':user.username}))
+        
+        return self.render_to_response({'username':username,'user_form':user_form,'contact_form':contact_form,})
+        
+            
